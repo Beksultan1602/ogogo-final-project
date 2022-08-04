7a235dbafdf4d07b8af54d59a1f8721a3379630e
Dastan Alymkulov <alymkulov58@gmail.com>
2022-08-04 19:19:02 +0600

    add navbar

---
+++ src/Layouts/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Button from '../components/UI/Button'
+
+const Header = () => {
+	const [headerItem, setHeaderItem] = useState([
+		'Мой ivi',
+		'Что нового',
+		'Фильмы',
+		'Сериалы',
+		'Мультфильмы',
+		'Тв-каналы',
+	])
+
+	return (
+		<div className='flex justify-between items-center pt-4'>
+			<ul className='flex gap-4'>
+				{headerItem.map(category => (
+					<li
+						className='font-semibold text-gray-500 hover:text-white transition cursor-pointer'
+						key={category}
+						onClick={() => category}
+					>
+						{category}
+					</li>
+				))}
+			</ul>
+			<div>
+				<Button>Войти или зарегистрироваться</Button>
+			</div>
+		</div>
+	)
+}
+
+export default Header
